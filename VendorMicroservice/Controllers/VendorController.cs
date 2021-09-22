92f275f9d5c22b3090facc6c33428bc5011dafb8
PradeepKumar04 <75063761+PradeepKumar04@users.noreply.github.com>
2021-09-22 16:55:12 +0530

    first commit

---
+++ VendorMicroservice/Controllers/VendorController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendorMicroservice.Data;
+using VendorMicroservice.Models;
+
+namespace VendorMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendorController : ControllerBase
+    {
+        private readonly VendorDbContext _db;
+
+        public VendorController(VendorDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("{product_Id}")]
+        public IActionResult getVendorDetails(int product_Id)
+        {
+            var result = _db.VendorStocks.Where(s => s.ProductId == product_Id && s.StockInHand > 0).OrderBy(s=>s.Vendor.DeliveryCharge).Select(s => new { VendorId = s.VendorId, VendorName = s.Vendor.VendorName, Rating = s.Vendor.Rating, DeliveryCharge = s.Vendor.DeliveryCharge });
+            return Ok(result);
+        }
+    }
+}
---
+++ VendorMicroservice/Data/VendorDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendorMicroservice.Models;
+
+namespace VendorMicroservice.Data
+{
+    public class VendorDbContext:DbContext
+    {
+        public DbSet<Vendor> Vendors { get; set; }
+        public DbSet<VendorStock> VendorStocks { get; set; }
+
+        public VendorDbContext(DbContextOptions<VendorDbContext> options):base(options)
+        {
+
+        }
+    }
+}
